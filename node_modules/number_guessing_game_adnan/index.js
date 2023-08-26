aca5d963bee97ac9b3c6e19fc4f2cc899e225c73
EponymousBearer <adnasir607@gmail.com>
2023-08-26 10:53:42 +0500

    none

---
+++ node_modules/number_guessing_game_adnan/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+const i = 0;
+for (let i = 0; i < 3; i++) {
+    const answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: "Guess Any Number Between 0 to 10: "
+        }
+    ]);
+    const { userGuess } = answers;
+    console.log("Your Guess is ", chalk.magenta(userGuess));
+    if (userGuess === systemGeneratedNo) {
+        console.log(chalk.green("Yesss! Your answer is correct"));
+        break;
+    }
+    else {
+        console.log(chalk.red("Please try again luck next time!"));
+    }
+    ;
+}
+console.log("The answer is ", systemGeneratedNo);
